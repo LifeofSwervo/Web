a604ce32be46d9397f3a0358cbd3274770425830
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-08-14 18:48:05 -0500

    Update App.jsx

--- React/Todo/src/App.jsx
+++ React/Todo/src/App.jsx
@@ -2,5 +2,16 @@ import "./styles.css"
 
 
 export default function App() {
-  return App;
+  return (
+    <> 
+      <form className="new-item-form">
+        <div className='form-row'>
+          <label htmlFor="item">New Task</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn"> Add Task</button>
+      </form>
+      <h1>Todo List</h1>
+    </>
+  )
 }
