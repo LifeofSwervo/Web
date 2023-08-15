03193bb8ad3437b849a399dd76902a9322f0616c
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-08-15 13:08:18 -0500

    Update App.jsx

--- React/Todo/src/App.jsx
+++ React/Todo/src/App.jsx
@@ -15,6 +15,24 @@ export default function App() {
       { id: crypto.randomUUID(), title: newItem, completed: false },
       ]
     })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
 
@@ -29,14 +47,17 @@ export default function App() {
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
-          return <li>
-          <label>
-            <input type="checkbox" checked={todo.completed} />
-              {todo.title}
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+          return (
+            <li key={todo.id}> // React uses key to understand which element to change
+              <label>
+                <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
+                  {todo.title}
+              </label>
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button> 
+            </li>
+          )
         })}
       </ul>
       <h1>Todo List</h1>
