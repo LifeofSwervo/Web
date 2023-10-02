2f140e79ea3b07d5f4e9cfb6917983a2a266b1df
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-10-02 09:16:25 -0500

    Chat application

--- Node/Chat application/server.js
+++ Node/Chat application/server.js
@@ -6,4 +6,8 @@ var server = app.listen(3000, () => {
     console.log('server is running on port: ', server.address().port);
 });
 
+var mongoose = require('mongoose');
+
+var dbUrl = ''
+
 app.use(express.static(__dirname))
