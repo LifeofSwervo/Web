274fc7b7864faae022be25adea1adf9e9e2c11c4
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-10-12 06:49:14 -0500

    Expense list and Item components

--- React/Budget App/src/App.jsx
+++ React/Budget App/src/App.jsx
@@ -5,6 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 const App = () => {
   return (
@@ -19,6 +20,12 @@ const App = () => {
         </div>
         <div className='col-sm'>
           <ExpenseTotal />
+        </div>
+      </div>
+      <h3 className='mt-3'>Expenses</h3>
+      <div className='row mt-3'>
+        <div className='col-sm'>
+          <ExpenseList />
         </div>
       </div>
     </div>
