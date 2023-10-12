7d840a80184d359fec12fc79db20e51e74066d1e
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-10-12 06:33:50 -0500

    Base budget details

---
+++ React/Budget App/src/components/ExpenseTotal.jsx
@@ -0,0 +1,10 @@
+
+const ExpenseTotal = () => {
+    return (
+        <div className='alert alert-primary'>
+            <span>Spent so far: $0</span>
+        </div>
+    );
+};
+
+export default ExpenseTotal;
---
+++ React/Budget App/src/components/Remaining.jsx
@@ -0,0 +1,10 @@
+
+const Remaining = () => {
+    return (
+        <div className='alert alert-success'>
+            <span>Remaining: $2000</span>
+        </div>
+    );
+};
+
+export default Remaining;
