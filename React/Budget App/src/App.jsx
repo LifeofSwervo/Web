27757e4b9c79fe7b6a4f79c351bb5107743b3edc
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-10-18 14:22:26 -0500

    Finalization

--- React/Budget App/src/App.jsx
+++ React/Budget App/src/App.jsx
@@ -12,32 +12,34 @@ import { AppProvider } from './context/AppContext';
 const App = () => {
   return (
     <AppProvider>
-    <div className='container'>
-      <h1 className='mt-3'>My Budget Planner</h1>
-      <div className='row mt-3'>
-        <div className='col-sm'>
-          <Budget />
+    <html /*className='bg-dark'*/>
+      <div className='container'>
+        <h1 className='mt-3'>My Budget Planner</h1>
+        <div className='row mt-3'>
+          <div className='col-sm'>
+            <Budget />
+          </div>
+          <div className='col-sm'>
+            <RemainingBudget />
+          </div>
+          <div className='col-sm'>
+            <ExpenseTotal />
+          </div>
         </div>
-        <div className='col-sm'>
-          <RemainingBudget />
+        <h3 className='mt-3'>Expenses</h3>
+        <div className='row'>
+          <div className='col-sm'>
+            <ExpenseList />
+          </div>
         </div>
-        <div className='col-sm'>
-          <ExpenseTotal />
+        <h3 className='mt-3'>Add Expense</h3>
+        <div className='row mt-3'>
+          <div className='col-sm'>
+            <AddExpenseForm />
+          </div>
         </div>
       </div>
-      <h3 className='mt-3'>Expenses</h3>
-      <div className='row'>
-        <div className='col-sm'>
-          <ExpenseList />
-        </div>
-      </div>
-      <h3 className='mt-3'>Add Expense</h3>
-      <div className='row mt-3'>
-        <div className='col-sm'>
-          <AddExpenseForm />
-        </div>
-      </div>
-    </div>
+    </html>
     </AppProvider>
   );
 };
