eb87f76e35a37777e73ce77b72ee74ecd1207795
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2023-11-20 07:27:17 -0600

    Tasks and Input update

--- React/TypeScript/ToDo/src/components/Input.tsx
+++ React/TypeScript/ToDo/src/components/Input.tsx
@@ -1,24 +1,44 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
-const InputContainer = () => {
-    const [newTask, setNewTask] = useState(""); 
+/**
+ * 
+ * @param handleSubmit - Handles the form submission. 
+ * @returns - Returns form and "Add Task" button.
+ */
+const InputContainer = ({
+    handleSubmit,
+} : {
+    handleSubmit: (e: FormEvent<HTMLFormElement>, value: string) => void;
+}) => {
+    const [newTaskName, setNewTaskName] = useState("");
     return (
-        <form action="" className="flex flex-col gap-4">
+        <form
+            action=""
+            className="flex flex-col gap-4"
+            onSubmit={(e) => {
+                handleSubmit(e, newTaskName); // Calls handleSubmit function with FormEvent and user taskName. 
+                setNewTaskName(""); // Clears input field
+            }}
+        >
             <div className="flex flex-col">
-                <label className="text-white">Enter your next task: </label>
-                <input className="p-1 rounded-sm"
-                type="text"
-                value={newTask}
-                onChange={(e) => setNewTask(e.target.value)} />
-            </div>
-            <button
-                type="submit"
-                className="bg-green-100 rounded-lg hover:bg-green-200 p-1"
-            >
-                Add Task
-            </button>
-        </form>
-    );
+                <label className="text-white">Enter your Task: </label>
+                {/* Input field for entering task name */}
+                <input
+                    className="p-1 rounded-sm"
+                    type="text"
+                    value={newTaskName}
+                    onChange={(e) => setNewTaskName(e.target.value)}
+                />
+                </div>
+                {/* Add Task button */}
+                <button
+                    type="submit"
+                    className="bg-green-100 rounded-lg hover:bg-green-200 p-1"
+                >
+                    Add Task
+                </button>   
+        </form>    
+    );   
 };
 
 export default InputContainer;
