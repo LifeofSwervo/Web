bd875ee1105bcc0342c867273034bc35d9dfaaa8
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-03-19 14:44:36 -0500

    Test

---
+++ React/Firebase Chat /firebase-chat/src/App.js
@@ -0,0 +1,17 @@
+// Firebase dependancies 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+firebase.initializeApp({
+  
+})
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
