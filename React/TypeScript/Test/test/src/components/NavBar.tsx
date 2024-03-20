c699ab0673087b2897042e495cd20ee7b1b3e942
Swervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-03-20 11:04:06 -0500

    NavBar

--- React/TypeScript/Test/test/src/components/NavBar.tsx
+++ React/TypeScript/Test/test/src/components/NavBar.tsx
@@ -1,11 +1,17 @@
 
 const NavBar = () => {
-    const ulStyle = {
+    const ulStyle: React.CSSProperties = {
         listStyleType: "none",
         margin: 0,
         padding: 0,
         overflow: "hidden",
         backgroundColor: "#333", // Black background of nav
+
+        // Fixes the navbar to the top of the page
+        position: "fixed",
+        width: "100%",
+        top: 0,
+        zIndex: 1,
     }
 
     const liStyle: React.CSSProperties = {
@@ -20,12 +26,18 @@ const NavBar = () => {
         padding: "14px 16px",
         textDecoration: "none"
     }
+
+    const namelessStyle: React.CSSProperties = {
+        float: "left"
+    }
+    
     return (
         <div>
             <ul id="navBarList" style={ulStyle}>
                 <li style={liStyle}><a href="#home" style={aStyle}>Home</a></li>
                 <li style={liStyle}><a href="#about" style={aStyle}>About</a></li>
                 <li style={liStyle}><a href="#contact" style={aStyle}>Contact Us</a></li>
+                <li style={namelessStyle}><a href="#nameless" style={aStyle}>Nameless Dev</a></li>
             </ul>
         </div>
     );
