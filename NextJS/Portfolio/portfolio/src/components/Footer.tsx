0f152fd3ee1bdb868e6556da3c083a2efdd0c39f
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-04-10 21:01:34 -0500

    Vectorized logo

--- NextJS/Portfolio/portfolio/src/components/Footer.tsx
+++ NextJS/Portfolio/portfolio/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Logo from "../../public/logo.svg";
+import Logo from "../../public/bitmap.svg";
 
 const footerStyle: React.CSSProperties = {
     backgroundColor: "#748386",
