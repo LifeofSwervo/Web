9f7ba0de0f7ac72079d7a1718de8dbbabad2af41
LifeofSwervo <82927987+LifeofSwervo@users.noreply.github.com>
2024-05-23 21:40:50 -0500

    Navbar and Projects mobile responsiveness

--- NextJS/Portfolio/portfolio/src/components/Home/Projects.tsx
+++ NextJS/Portfolio/portfolio/src/components/Home/Projects.tsx
@@ -1,15 +1,7 @@
 import React from "react"
 import ProjectsShowcase from "./ProjectsShowcase"
 import placeholderImage from "../../Assets/placeholder.png";
-
-const projectsStyle: React.CSSProperties = {
-    alignItems: "center",
-    justifyContent: "center",
-    display: "flex",
-    flexDirection: "column",
-    zIndex: 3,
-    color: "#D2FDFF"
-}
+import "./Projects.css";
 
 const showcaseDescriptionStyle: React.CSSProperties = {
     marginBottom: "10vh"
@@ -46,12 +38,9 @@ const SpaceInvaders2 = [
     "/Assets/SpaceInvaders/SpaceInvadersCode.png",
 ]
 
-
-const bistroCafeDescription = "The Bistro Cafe is was one of my first website&#39;s made purely from HTML, CSS and Javascript. This was my introduction into webdevelopment. This helped me pick up the very basics of web development. For this project I gained experience making the logo&#39;s from scratch."
-
 const Projects = () => {
     return (
-        <div style={projectsStyle}>
+        <div className="projects">
             <h1>Projects I&#39;ve made.</h1>
             <p style={showcaseDescriptionStyle}>This is a showcase of the various different projects I have completed.</p>
             <div>
--- NextJS/Portfolio/portfolio/src/components/NavBar.tsx
+++ NextJS/Portfolio/portfolio/src/components/NavBar.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from 'next/link';
 import React, { useState } from 'react';
 import Logo from "../../public/Logos/navBarLogo.svg";
+import "./NavBar.css";
 
 
 const goldColor = '#F7D002'; 
@@ -13,57 +14,26 @@ const NavBar = () => {
     const [homeHover, setHomeHover] = React.useState(false);
     const [aboutHover, setAboutHover] = React.useState(false);
     const [contactHover, setContactHover] = React.useState(false);
- 
-    const navStyle: React.CSSProperties = {
-        backgroundColor: "#0C1618",
-        padding: '0',
-        margin: '0',
-        lineHeight: '0',
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-
-        // Breakpoint 
-        listStyleType: "none",
-        overflow: "hidden",
-        borderBottom: "2px solid #D2FDFF",
-        position: "fixed",
-        width: "100%",
-        top: 0,
-        zIndex: 4,
-    }
-
-    const ulStyle: React.CSSProperties = {
-        listStyleType: "none",
-        padding: '0.5%',
-        overflow: "hidden",
-        margin: 'auto 5em auto auto',
-        alignItems: "center",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: 'center',
-        minHeight: '2em',
-    }
 
     const aboutStyle: React.CSSProperties = {
         float: "right",
         textDecoration: aboutHover ? 'underline' : 'none',
         color: aboutHover ? `${goldColor}` : 'white',
-        margin: 'auto 3vw'
+        margin: 'auto 2vw'
     }
 
     const homeStyle: React.CSSProperties = {
         float: "right",
         textDecoration: homeHover ? 'underline' : 'none',
         color: homeHover ? `${goldColor}` : 'white',
-        margin: 'auto 3vw'
+        margin: 'auto 2vw'
     }
 
     const contactStyle: React.CSSProperties = {
         float: "right",
         color: contactHover ? `${goldColor}` : 'white',
         textDecoration: contactHover ? 'underline' : 'none',
-        margin: 'auto 3vw'
+        margin: 'auto 2vw'
     }
 
     const linksStyle: React.CSSProperties = {
@@ -80,12 +50,12 @@ const NavBar = () => {
     }
     
     return (
-        <nav style={navStyle}>
+        <nav className="navStyle">
             <a 
                 href="/">   
                 <Image src={Logo} alt="Logo" style={imageStyle} /> 
             </a>
-            <ul style={ulStyle}>
+            <ul className="ulStyle">
                 <li 
                 style={linksStyle} 
                 onMouseEnter={() => setHomeHover(true)} 
